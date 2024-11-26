5967938e46bb377c546b823ada4161167b6ca3de
Tipok007 <lost.tank.as@gmail.com>
2024-11-27 00:18:44 +0500

    добавлена обработка исключений

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
-from http.client import responses
 from tkinter import *
-from tkinter import filedialog as fd
-from tkinter ttk
 import requests
-
+from tkinter import filedialog as fd
+from tkinter import messagebox as mb
+from tkinter import ttk
 
 def upload():
-    filepath = fd. askopenfilename()
-    if filepath:
-        files = {'file': open(filepath, 'rb')}
-        filesresponse = requests.post("https://file.io", files=files)
-        if response.status_code== 200:
-            link = response.json()['link']
-            entry.insert(0, link)
-
+    try:
+        filepath = fd.askopenfilename()
+        if filepath:
+            with open(filepath, 'rb') as f:
+                files = {'file': f}
+                response = requests.post('https://file.io', files=files)
+                response.raise_for_status()  # Проверка на ошибки HTTP
+                download_link = response.json().get('link')
+                if download_link:
+                    entry.delete(0, END)
+                    entry.insert(0, download_link)
+                else:
+                    raise ValueError("Не удалось получить ссылку для скачивания")
+    except ValueError as ve:
+        mb.showerror("Ошибка", f"Произошла ошибка: {ve}")
+    except Exception as e:
+        mb.showerror("Ошибка", f"Произошла ошибка: {e}")
 
 
 window = Tk()
-window.title('Сохранение файлов в облаке')
-window.geometry('400x200')
+window.title("Сохранение файлов в облаке")
+window.geometry("360x100")
 
-button = ttk.button(text='Загрузить файл', command=upload)
-button.pack()
+upload_button = ttk.Button(text="Загрузить файл", command=upload)
+upload_button.pack()
 
 entry = ttk.Entry()
 entry.pack()
