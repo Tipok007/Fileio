909f79a129f0dcdbc30ed44ff3e6e372171b0240
Tipok007 <lost.tank.as@gmail.com>
2024-11-27 00:30:40 +0500

    Сохранение ссылок в формате JSON

--- main.py
+++ main.py
@@ -1,38 +1,61 @@
-from tkinter import *
+import json
+import os
 import requests
 import pyperclip
-from tkinter import filedialog as fd
-from tkinter import messagebox as mb
+from tkinter import filedialog, messagebox, Toplevel, Listbox, Scrollbar, HORIZONTAL, VERTICAL
 from tkinter import ttk
+import tkinter as tk
 
-def upload():
+history_file = "upload_history.json"
+
+
+def save_history(file_path, download_link):
+    history = []
+    if os.path.exists(history_file):
+        with open(history_file, "r") as file:
+            history = json.load(file)
+
+    history.append({"file_path": os.path.basename(file_path), "download_link": download_link})
+
+    with open(history_file, "w") as file:
+        json.dump(history, file, indent=4)
+
+
+def upload_file():
     try:
-        filepath = fd.askopenfilename()
+        filepath = filedialog.askopenfilename()
         if filepath:
             with open(filepath, 'rb') as f:
                 files = {'file': f}
                 response = requests.post('https://file.io', files=files)
-                response.raise_for_status()  # Проверка на ошибки HTTP
+                response.raise_for_status()
                 download_link = response.json().get('link')
                 if download_link:
-                    entry.delete(0, END)
-                    entry.insert(0, download_link)
+                    link_entry.delete(0, tk.END)
+                    link_entry.insert(0, download_link)
+                    pyperclip.copy(download_link)
+                    save_history(filepath, download_link)
+                    messagebox.showinfo("Ссылка скопирована", "Ссылка успешно скопирована в буфер обмена")
                 else:
                     raise ValueError("Не удалось получить ссылку для скачивания")
+    except requests.RequestException as e:
+        messagebox.showerror("Ошибка сети", f"Произошла ошибка сети: {e}")
     except ValueError as ve:
-        mb.showerror("Ошибка", f"Произошла ошибка: {ve}")
-    except Exception as e:
-        mb.showerror("Ошибка", f"Произошла ошибка: {e}")
+        messagebox.showerror("Ошибка", str(ve))
+    except Exception as ex:
+        messagebox.showerror("Ошибка", f"Произошла неизвестная ошибка: {ex}")
 
 
-window = Tk()
-window.title("Сохранение файлов в облаке")
-window.geometry("360x100")
+app = tk.Tk()
+app.title("TempFile Share")
+app.geometry("360x100")
 
-upload_button = ttk.Button(text="Загрузить файл", command=upload)
+upload_button = ttk.Button(app, text="Upload File", command=upload_file)
 upload_button.pack()
 
-entry = ttk.Entry()
-entry.pack()
+link_entry = ttk.Entry(app)
+link_entry.pack()
 
-window.mainloop()
+app.mainloop()
+
+
