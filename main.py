e29e0e5845e5983843cf5ed4627d5b3df7ab7f02
Tipok007 <lost.tank.as@gmail.com>
2024-11-27 00:36:58 +0500

    Загрузка истории из JSON

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
 import os
 import requests
-import pyperclip
-from tkinter import filedialog, messagebox, Toplevel, Listbox, Scrollbar, HORIZONTAL, VERTICAL
-from tkinter import ttk
+from tkinter import *
+from tkinter import ttk, messagebox
 import tkinter as tk
+from tkinter import filedialog as fd
 
 history_file = "upload_history.json"
 
@@ -21,41 +21,59 @@ def save_history(file_path, download_link):
         json.dump(history, file, indent=4)
 
 
+def show_history():
+    if not os.path.exists(history_file):
+        messagebox.showinfo("История", "История загрузок пуста")
+        return
+
+    history_window = Toplevel(app)
+    history_window.title("История Загрузок")
+
+    files_listbox = Listbox(history_window, width=50, height=20)
+    files_listbox.grid(row=0, column=0, padx=(10, 0), pady=10)
+
+    links_listbox = Listbox(history_window, width=50, height=20)
+    links_listbox.grid(row=0, column=1, padx=(0, 10), pady=10)
+
+    with open(history_file, "r") as file:
+        history = json.load(file)
+        for item in history:
+            files_listbox.insert(END, item['file_path'])
+            links_listbox.insert(END, item['download_link'])
+
+
 def upload_file():
-    try:
-        filepath = filedialog.askopenfilename()
-        if filepath:
-            with open(filepath, 'rb') as f:
-                files = {'file': f}
-                response = requests.post('https://file.io', files=files)
-                response.raise_for_status()
-                download_link = response.json().get('link')
-                if download_link:
-                    link_entry.delete(0, tk.END)
-                    link_entry.insert(0, download_link)
-                    pyperclip.copy(download_link)
-                    save_history(filepath, download_link)
-                    messagebox.showinfo("Ссылка скопирована", "Ссылка успешно скопирована в буфер обмена")
+    filepath = fd.askopenfilename()
+    if filepath:
+        try:
+            with open(filepath, 'rb') as file:
+                files = {'file': file}
+                response = requests.post("https://file.io", files=files)
+
+                if response.status_code == 200:
+                    link = response.json().get('link')
+                    if link:
+                        messagebox.showinfo("Успех", f"Файл загружен! Ссылка: {link}")
+                        save_history(filepath, link)
+                    else:
+                        messagebox.showerror("Ошибка", "Не удалось получить ссылку на файл.")
                 else:
-                    raise ValueError("Не удалось получить ссылку для скачивания")
-    except requests.RequestException as e:
-        messagebox.showerror("Ошибка сети", f"Произошла ошибка сети: {e}")
-    except ValueError as ve:
-        messagebox.showerror("Ошибка", str(ve))
-    except Exception as ex:
-        messagebox.showerror("Ошибка", f"Произошла неизвестная ошибка: {ex}")
+                    messagebox.showerror("Ошибка", "Ошибка при загрузке файла.")
+        except Exception as e:
+            messagebox.showerror("Ошибка", f"Произошла ошибка: {str(e)}")
 
 
 app = tk.Tk()
 app.title("TempFile Share")
-app.geometry("360x100")
 
-upload_button = ttk.Button(app, text="Upload File", command=upload_file)
-upload_button.pack()
+upload_button = ttk.Button(app, text="Загрузить файл", command=upload_file)
+upload_button.pack(pady=10)
 
-link_entry = ttk.Entry(app)
-link_entry.pack()
+history_button = ttk.Button(app, text="Показать Историю", command=show_history)
+history_button.pack(pady=10)
 
 app.mainloop()
+
+
 
 
